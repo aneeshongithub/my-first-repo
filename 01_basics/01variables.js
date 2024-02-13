c7f43d7d340267669b12fe9c60128dd80e376d84
aneeshongithub <159943649+aneeshongithub@users.noreply.github.com>
2024-02-13 18:37:19 +0000

    created an new file and test somthing nwww in variable s

---
+++ 01_basics/01variables.js
@@ -0,0 +1,8 @@
+const accountId = 43434
+let accountemail = "aneeshkush@gamil.com"
+var accountpass = "234324"
+accountcity = "faridabad"
+
+//accountId = 545
+console.log(accountId);
+console.table([accountId,accountpass,accountcity])
